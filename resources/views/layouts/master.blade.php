5f107d44d3995328484b65167a7de4a7d3c21eb2
Agustín Nieto García <anietog1@eafit.edu.co>
2020-02-11 17:59:15 -0500

    Bootstrap sin 3eros

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,8 +6,8 @@
     <meta charset="utf-8">
     <title>@yield('title','Home Page')</title>
     <!-- Styles -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="{{ asset('css/customStyle.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 
 <body>
