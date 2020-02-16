04107635396e1b1642acbde861c55e280fe9f102
Agustín Nieto García <anietog1@eafit.edu.co>
2020-02-15 22:34:51 -0500

    5

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,6 +18,10 @@
                     {{ config('app.name', 'Laravel') }}
                 </a>
 
+                <a class="navbar-brand" href="{{ route('product.show', ['id' => 121]) }}">
+                    {{ config('PRODUCT121', 'PRODUCT121') }}
+                </a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
