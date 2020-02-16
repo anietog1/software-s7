2539c2216f78246e4233a82294a1cabc1a26a905
Agustín Nieto García <anietog1@eafit.edu.co>
2020-02-15 22:43:15 -0500

    7

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class ProductController extends Controller
 {
     public function show($id)
@@ -17,6 +19,24 @@ class ProductController extends Controller
         $data["product"] = $listProducts[$id];
         $data["sizes"] = $listOfSizes;
         return view('product.show')->with("data",$data);
+    }
+
+    public function create()
+    {
+        $data = []; //to be sent to the view
+        $data["title"] = "Create product";
+
+        return view('product.create')->with("data",$data);
+    }
+
+    public function save(Request $request)
+    {
+        $request->validate([
+            "name" => "required",
+            "price" => ["required", "gt:0"]
+        ]);
+        dd($request->all());
+        //here goes the code to call the model and save it to the database
     }
 }
 
